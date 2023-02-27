082029e6e443ac1b9493776daf991503bc414312
tanmay002 <75234031+tanmay002@users.noreply.github.com>
2023-02-27 15:03:53 +0530

    Add files via upload

---
+++ mak1.py
@@ -0,0 +1,11 @@
+m,n=map(int,input().split())
+
+count=0
+arr=[]
+arr= [[0]*m]*n 
+
+print(arr)
+
+count=(m-1)+(n-1)
+
+print("minimum cuts",count)
---
+++ mak2.py
@@ -0,0 +1,5 @@
+
+T,a,b= map(int, input().split())
+
+
+print(a+1,b+1)
